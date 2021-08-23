1346b8a26206e9575723d286118f6be0fe2bcd3d
Andre Linces <andrelinces@yahoo.com.br>
2021-08-22 21:00:06 -0300

    Criando nova classe para a segunda view controller.

---
+++ Passando Dados/detalhesVViewController.swift
@@ -0,0 +1,29 @@
+//
+//  detalhesVViewController.swift
+//  Passando Dados
+//
+//  Created by Andre Linces on 22/08/21.
+//
+
+import UIKit
+
+class detalhesVViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
